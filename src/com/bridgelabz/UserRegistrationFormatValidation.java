507ff8b4f89e6ed607d4ed32240de3697f718107
yogkri <dharmayog91@gmail.com>
2021-10-10 15:01:40 +0530

    Adding User Registration Format For Email ID Validation

--- src/com/bridgelabz/UserRegistrationFormatValidation.java
+++ src/com/bridgelabz/UserRegistrationFormatValidation.java
@@ -6,15 +6,27 @@ import java.util.regex.Pattern;
 public class UserRegistrationFormatValidation {
     Matcher matcher;
     Pattern pattern;
-    String Regex = "^[A-Z]{1}+[a-z]{2,}\\s[A-Z]{1}[a-z]{2,}"; //User Registration Pattern For First Name
+    String RegexName = "^[A-Z]{1}+[a-z]{2,}\\s[A-Z]{1}[a-z]{2,}"; //User Registration Pattern For First Name And Last Name
+    String RegexEmail = "^[A-Za-z0-9.]+@[a-z.]+[a-z]{1,3}$"; //User Registration Pattern For Email ID
 
+    //Created A Method To Check User Name Validation With String RegexName
     public void nameValidation(String name) {
-        pattern = Pattern.compile(Regex);
+        pattern = Pattern.compile(RegexName);
         matcher = pattern.matcher(name);
-        boolean val = matcher.matches();
-        System.out.println(val);
-        if(val == false){
+        boolean validation = matcher.matches();
+        System.out.println(validation);
+        if(validation == false){
             System.out.println("Name Should Begin With Capital Letter, Have Minimum 3 Letters And Also Have Space Between First And Last Name ");
+        }
+    }
+    //Created A Method To Check User Email ID Validation With String RegexEmail
+    public void validateOfEmail(String email){
+        pattern = Pattern.compile(RegexEmail);
+        matcher = pattern.matcher(email);
+        Boolean validation = matcher.matches();
+        System.out.println(validation);
+        if(validation == false){
+            System.out.println("Email A Valid Email ID According To Pattern");
         }
     }
 }
--- src/com/bridgelabz/UserRegistrationFormatValidationMain.java
+++ src/com/bridgelabz/UserRegistrationFormatValidationMain.java
@@ -7,10 +7,14 @@ public class UserRegistrationFormatValidationMain {
         System.out.println("\n****Welcome To User Registration Format Validation Program****");
         UserRegistrationFormatValidation Validation = new UserRegistrationFormatValidation();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter First Name And Last Name For Validation");
+        System.out.println("Enter First NAmer And Last Name For Validation");
         String name = scanner.nextLine();
         Validation.nameValidation(name);
+        System.out.println("Enter the email to validation");
+        String res = scanner.nextLine();
+        Validation.validateOfEmail(res);
     }
 }
+
 
 
