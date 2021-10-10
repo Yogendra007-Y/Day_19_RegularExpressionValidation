7dbfd0566e7988ab4ce28cbcfaab55375d241110
yogkri <dharmayog91@gmail.com>
2021-10-10 15:11:40 +0530

    Adding User Registration Format For Password Validation

--- src/com/bridgelabz/UserRegistrationFormatValidation.java
+++ src/com/bridgelabz/UserRegistrationFormatValidation.java
@@ -9,36 +9,46 @@ public class UserRegistrationFormatValidation {
     String RegexName = "^[A-Z]{1}+[a-z]{2,}\\s[A-Z]{1}[a-z]{2,}"; //User Registration Pattern For First Name And Last Name
     String RegexEmail = "^[A-Za-z0-9.]+@[a-z.]+[a-z]{1,3}$"; //User Registration Pattern For Email ID
     String RegexPhoneNo = "^[0-9]{2}\\s[0-9]{10}$"; //User Registration Pattern For Phone Number
+    String RegexPassword = "^[A-Za-z0-9]{8,}"; //User Registration Pattern For Password
 
-
+    public void validationOfRegex(String regex,String name){
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(name);
+    }
     //Created A Method To Check UserName Validation With String RegexName
     public void nameValidation(String name) {
-        pattern = Pattern.compile(RegexName);
-        matcher = pattern.matcher(name);
-        boolean validation = matcher.matches();
-        System.out.println(validation);
-        if(validation == false){
-            System.out.println("Name Should Begin With Capital Letter, Have Minimum 3 Letters And Also Have Space Between First And Last Name ");
+        validationOfRegex(RegexName,name);
+        boolean validationName = matcher.matches();
+        System.out.println(validationName);
+        if(!validationName){
+            System.out.println("Enter Name According To Pattern: [A-Z]{1}+[a-z]{2,}\\s[A-Z]{1}[a-z]{2,} ");
         }
     }
     //Created A Method To Check User Email ID Validation With String RegexEmail
     public void emailIdValidation(String email){
-        pattern = Pattern.compile(RegexEmail);
-        matcher = pattern.matcher(email);
-        Boolean validation = matcher.matches();
-        System.out.println(validation);
-        if(validation == false){
-            System.out.println("Enter A Valid Email ID According To Pattern");
+        validationOfRegex(RegexEmail,email);
+        Boolean validationEmail = matcher.matches();
+        System.out.println(validationEmail);
+        if(!validationEmail){
+            System.out.println("Enter Email ID According To Pattern: [A-Za-z0-9.]+@[a-z.]+[a-z]{1,3}");
         }
     }
-    //Created A Method To Check User Email ID Validation With String RegexPhoneNo
+    //Created A Method To Check User Phone Number Validation With String RegexPhoneNo
     public void phoneNumberValidation(String phoneNumber){
-        pattern = Pattern.compile(RegexPhoneNo);
-        matcher = pattern.matcher(phoneNumber);
-        boolean val = matcher.matches();
-        System.out.println(val);
-        if(val == false){
-            System.out.println("number should have ten digit word");
+        validationOfRegex(RegexPhoneNo,phoneNumber);
+        boolean validationPhoneNo = matcher.matches();
+        System.out.println(validationPhoneNo);
+        if(!validationPhoneNo){
+            System.out.println("Enter Phone Number According TO Pattern: [0-9]{2}\\s[0-9]{10} ");
+        }
+    }
+    //Created A Method To Check User Password Validation With String RegexPassword
+    public void passwordValidation(String password){
+        validationOfRegex(RegexPassword, password);
+        boolean validationPassword = matcher.matches();
+        System.out.println(validationPassword);
+        if(!validationPassword) {
+            System.out.println("Enter Password According To Pattern: [a-zA-Z0-9]{8,}");
         }
     }
 }
--- src/com/bridgelabz/UserRegistrationFormatValidationMain.java
+++ src/com/bridgelabz/UserRegistrationFormatValidationMain.java
@@ -7,15 +7,23 @@ public class UserRegistrationFormatValidationMain {
         System.out.println("\n****Welcome To User Registration Format Validation Program****");
         UserRegistrationFormatValidation Validation = new UserRegistrationFormatValidation();
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter First Name And Last Name For Validation");
         String name = scanner.nextLine();
         Validation.nameValidation(name);
+
         System.out.println("Enter Email ID For Validation");
         String emailId = scanner.nextLine();
         Validation.emailIdValidation(emailId);
-        System.out.println("Enter Phone Number For");
-        String number = scanner.nextLine();
-        Validation.phoneNumberValidation(number);
+
+        System.out.println("Enter Phone Number For Validation");
+        String phoneNumber = scanner.nextLine();
+        Validation.phoneNumberValidation(phoneNumber);
+
+        System.out.println("Enter Password For Validation");
+        String password = scanner.nextLine();
+        Validation.passwordValidation(password);
+
     }
 }
 
